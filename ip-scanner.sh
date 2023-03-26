178e359ca2e9e1fddcf887ed121f2cde3d8dbd7d
Vamsi Nukavarapu <77102072+vamsi-120304@users.noreply.github.com>
2023-03-26 12:04:04 +0530

    Update ip-scanner.sh

--- ip-scanner.sh
+++ ip-scanner.sh
@@ -11,6 +11,7 @@ else
 	chmod +x sweep.sh
 	./sweep.sh $1 > /tmp/qwerty.txt
 	sleep 10s
-	cat /tmp/qwerty.txt | uniq > ip
-	cat -n ip
+	cat /tmp/qwerty.txt | uniq > ip.txt
+	cat -n ip.txt
+	rm /tmp/qwerty.txt
 fi
